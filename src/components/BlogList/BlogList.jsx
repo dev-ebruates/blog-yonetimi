da66bf801e5fcb329d6eeeadd8edd69cda4b80c0
dev-ebruates <devatesebru@gmail.com>
2024-11-02 20:58:36 +0300

    Blog yazılarında filtreleme ve arama

--- src/components/BlogList/BlogList.jsx
+++ src/components/BlogList/BlogList.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import BlogItem from "./BlogItem";
 import PropTypes from "prop-types";
+import './BlogList.css'
 
 
 const BlogList = ({blogPosts}) => {
  
+  // Arama terimi için state tanımla
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Arama kutusuna yazılan değeri güncelleyen fonksiyon
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filtreleme fonksiyonu: girilen terimi içeren öğeleri döndürür
+  const filteredBlogPosts = blogPosts.filter(item =>
+     (item.baslik + " " + item.icerik + " " + item.yazar + " " + item.tarih).toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
       <div className="blog-list-container">
+        <input 
+        type="text"
+        placeholder="Aramak için yazın..."
+        value={searchTerm}
+        onChange={handleSearch}
+        className="search-input"
+      />
          
-          {blogPosts.map((post, index) => (
+          {filteredBlogPosts.map((post, index) => (
               <BlogItem
                   key={index}
                   baslik={post.baslik}
