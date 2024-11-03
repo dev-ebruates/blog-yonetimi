f75bb7d79bd4c9d92a985cd8e22860f327bd9be4
dev-ebruates <devatesebru@gmail.com>
2024-11-03 19:10:00 +0300

    delete-edit

--- src/components/BlogList/BlogItem.jsx
+++ src/components/BlogList/BlogItem.jsx
@@ -1,14 +1,25 @@
-import PropTypes from 'prop-types';
-import './BlogItem.css';
+import PropTypes from "prop-types";
+import "./BlogItem.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const BlogItem = ({ baslik, icerik, yazar, tarih }) => {
   return (
-      <div className='card'>
-          <h2>{baslik}</h2>
-          <p>{icerik}</p>
-          <p className='author' >Yazar: {yazar}</p>
-          <p className='date' >Tarih: {tarih}</p>
+    <div className="card">
+      <h2>{baslik}</h2>
+      <p>{icerik}</p>
+      <p className="author">Yazar: {yazar}</p>
+      <p className="date">Tarih: {tarih}</p>
+
+      <div className="button-container">
+        <button title="Edit" className="btn icon-button">
+          <span className="fas fa-edit custom-icon icon"></span>
+        </button>
+        <button title="Delete" className=" btn icon-button">
+        <span className="fas fa-trash custom-icon icon"></span>
+        </button>
       </div>
+    </div>
   );
 };
 
@@ -16,7 +27,7 @@ BlogItem.propTypes = {
   baslik: PropTypes.string.isRequired,
   icerik: PropTypes.string.isRequired,
   yazar: PropTypes.string.isRequired,
-  tarih: PropTypes.string.isRequired
+  tarih: PropTypes.string.isRequired,
 };
 
-export default BlogItem
+export default BlogItem;
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import './Button.css'; 
 
+
 const Button = ({ 
     label, 
     onClick, 
