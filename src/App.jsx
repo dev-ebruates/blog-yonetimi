ef69a911c3f40a0eba80bb852b8959121a7c9c30
dev-ebruates <devatesebru@gmail.com>
2024-11-03 23:57:20 +0300

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,49 @@
-import BlogList from "./components/BlogList/BlogList"
-import blogImages from './images/blogImages.jpg'; 
-import './App.css'
+import BlogList from "./components/BlogList/BlogList";
+import blogImages from "./images/blogImages.jpg";
+import "./App.css";
 import AddNewBlog from "./components/BlogList/AddNewBlog";
 import { useState } from "react";
 import staticBlogData from "./data/blogData";
 
 const App = () => {
-
- 
-
-//DELETE
+  //DELETE
   const handleDeleteBlog = (id) => {
-    const updatedBlogPosts = blogPosts.filter(blog => blog.id !== id);
-    setBlogPosts(updatedBlogPosts)
+    const updatedBlogPosts = blogPosts.filter((blog) => blog.id !== id);
+    setBlogPosts(updatedBlogPosts);
     localStorage.setItem("localBlogData", JSON.stringify(updatedBlogPosts));
   };
   // localStorage.clear();
-  let blogData = localStorage.getItem("localBlogData") === null ? null : JSON.parse(localStorage.getItem("localBlogData"));
-  if(blogData === null){
+  let blogData =
+    localStorage.getItem("localBlogData") === null
+      ? null
+      : JSON.parse(localStorage.getItem("localBlogData"));
+  if (blogData === null) {
     blogData = staticBlogData;
     localStorage.setItem("localBlogData", JSON.stringify(blogData));
   }
   const [blogPosts, setBlogPosts] = useState(blogData);
-  const [isSorted, setIsSorted] = useState(false);
-  
-  
 
   const handleAddBlog = (newBlog) => {
-      blogPosts.push(newBlog);
-      setBlogPosts([...blogPosts]);
-      console.log(newBlog);
-      console.log(blogPosts);
-      localStorage.setItem("localBlogData", JSON.stringify(blogPosts));
+    blogPosts.push(newBlog);
+    setBlogPosts([...blogPosts]);
+    console.log(newBlog);
+    console.log(blogPosts);
+    localStorage.setItem("localBlogData", JSON.stringify(blogPosts));
   };
   const sortByDate = () => {
-    const sortedPosts = [...blogPosts].sort((a, b) => new Date(b.tarih) - new Date(a.tarih));
+    const sortedPosts = [...blogPosts].sort(
+      (a, b) => new Date(b.tarih) - new Date(a.tarih)
+    );
     setBlogPosts(sortedPosts);
-    setIsSorted(true);
   };
   return (
     <div>
-      <img className="blogImages"  src={blogImages} alt="blogImages"/>
-      <button onClick={sortByDate}>
-        {isSorted ? 'Tarihe Göre Sıralı' : 'Tarihe Göre Sırala'}
-      </button>
-      <BlogList blogPosts={blogPosts} handleDeleteBlog={handleDeleteBlog}/>
+      <img className="blogImages" src={blogImages} alt="blogImages" />
+      <button onClick={sortByDate}>Tarihe göre sırala</button>
+      <BlogList blogPosts={blogPosts} handleDeleteBlog={handleDeleteBlog} />
       <AddNewBlog onAddBlog={handleAddBlog} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
