e4c3dacc8fd08a46005089f09f5139652649daee
dev-ebruates <devatesebru@gmail.com>
2024-11-06 02:39:37 +0300

    revert button

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import './Button.css'; 
+
+
+const Button = ({ 
+    label, 
+    onClick, 
+    type = 'button', 
+    disabled = false, 
+    color = 'primary', // Varsayılan renk
+    size = 'medium' // Varsayılan boyut
+}) => {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+            className={`custom-button ${color} ${size}`} // Renk ve boyut sınıflarını ekle
+        >
+            {label}
+        </button>
+    );
+};
+
+Button.propTypes = {
+    label: PropTypes.string,
+    onClick: PropTypes.func,
+    type: PropTypes.string,
+    disabled: PropTypes.bool,
+    color: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger']), // Renk seçenekleri
+    size: PropTypes.oneOf(['small', 'medium', 'large']), // Boyut seçenekleri
+};
+
+export default Button;
